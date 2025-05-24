17a48d18e3ccca8919d1605de8856e74529dde55
Tomate <louis44.boucard@gmail.com>
2025-05-24 12:39:15 +0200

    sd

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -18,105 +18,41 @@ const Dashboard = () => {
     >
       <Container maxWidth="lg">
         <Grid container spacing={{ xs: 3, sm: 4, md: 5 }}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            sx={{ mb: { xs: 3, md: 0 } }}
-          >
-            <Paper
-              elevation={3}
-              sx={{
-                p: { xs: 3, md: 4 },
-                height: { xs: 300, md: 350 },
-                display: 'flex',
-                flexDirection: 'column',
-                borderRadius: 4, // arrondi des bords
-              }}
+          {[ 
+            { component: <BarChart /> },
+            { component: <TotalDeathsChart /> },
+            { component: <PieChart /> },
+            { component: <PieChart2 /> },
+            { component: <TotalRecoveredChart /> },
+            { component: <DailyCasesChart /> },
+          ].map((chart, index) => (
+            <Grid
+              item
+              key={index}
+              xs={12}
+              sm={index > 1 ? 6 : 12}
+              md={index > 1 ? 3 : 6}
+              sx={{ mb: { xs: 3, sm: 0 } }}
             >
-              <BarChart />
-            </Paper>
-          </Grid>
-
-          <Grid
-            item
-            xs={12}
-            md={6}
-            sx={{ mb: { xs: 3, md: 0 } }}
-          >
-            <Paper
-              elevation={3}
-              sx={{
-                p: { xs: 3, md: 4 },
-                height: { xs: 300, md: 350 },
-                display: 'flex',
-                flexDirection: 'column',
-                borderRadius: 4,
-              }}
-            >
-              <TotalDeathsChart />
-            </Paper>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3} sx={{ mb: { xs: 3, sm: 0 } }}>
-            <Paper
-              elevation={3}
-              sx={{
-                p: { xs: 2, sm: 3, md: 4 },
-                height: { xs: 250, sm: 280, md: 300 },
-                display: 'flex',
-                flexDirection: 'column',
-                borderRadius: 4,
-              }}
-            >
-              <PieChart />
-            </Paper>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3} sx={{ mb: { xs: 3, sm: 0 } }}>
-            <Paper
-              elevation={3}
-              sx={{
-                p: { xs: 2, sm: 3, md: 4 },
-                height: { xs: 250, sm: 280, md: 300 },
-                display: 'flex',
-                flexDirection: 'column',
-                borderRadius: 4,
-              }}
-            >
-              <PieChart2 />
-            </Paper>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3} sx={{ mb: { xs: 3, sm: 0 } }}>
-            <Paper
-              elevation={3}
-              sx={{
-                p: { xs: 2, sm: 3, md: 4 },
-                height: { xs: 250, sm: 280, md: 300 },
-                display: 'flex',
-                flexDirection: 'column',
-                borderRadius: 4,
-              }}
-            >
-              <TotalRecoveredChart />
-            </Paper>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3} sx={{ mb: { xs: 3, sm: 0 } }}>
-            <Paper
-              elevation={3}
-              sx={{
-                p: { xs: 2, sm: 3, md: 4 },
-                height: { xs: 250, sm: 280, md: 300 },
-                display: 'flex',
-                flexDirection: 'column',
-                borderRadius: 4,
-              }}
-            >
-              <DailyCasesChart />
-            </Paper>
-          </Grid>
+              <Paper
+                elevation={3}
+                sx={{
+                  p: { xs: 2, sm: 3, md: 4 },
+                  height: { xs: 280, sm: 320, md: 350 },
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 4,
+                  overflow: 'hidden',
+                }}
+              >
+                <Box sx={{ width: '100%', height: '100%' }}>
+                  {chart.component}
+                </Box>
+              </Paper>
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </Box>
